b0ad4e63625e797b6a5b3fbabaab4474958d8d85
Dan George <dangeorge35@gmail.com>
2025-01-01 11:32:23 +0100

    theme modification

--- src/Components/ThemeToggle.jsx
+++ src/Components/ThemeToggle.jsx
@@ -3,18 +3,27 @@ import { HiSun, HiMoon } from "react-icons/hi";
 import { ThemeContext } from "../context/ThemeContext";
 
 const ThemeToggle = () => {
-  const {theme, setTheme} = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext);
+
   return (
-    <div className="p-2 ">
-      {theme === "dark" ? 
-      (
-        <div className="flex items-center cursor-pointer" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-          <HiSun className="text-primary text-2xl mr-2 "/> Light Mode
+    <div className="p-2 align-middle">
+      {theme === "dark" ? (
+        <div
+          className="flex items-center cursor-pointer  bg-gray-200 text-black py-2 px-3 rounded-full hover:bg-gray-300 transition-all duration-300"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          <HiSun className="text-sky text-2xl" />
+          <span className="hidden sm:inline-block ml-3 text-sm">
+            Light Mode
+          </span>
         </div>
-      ) :
-       (
-        <div className="flex items-center cursor-pointer" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-          <HiMoon className="text-primary text-2xl mr-2 "/> Dark Mode
+      ) : (
+        <div
+          className="flex items-center cursor-pointer  bg-gray-800 text-white py-2 px-3 rounded-full hover:bg-gray-600 transition-all duration-300"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          <HiMoon className="text-sky text-2xl" />
+          <span className="hidden sm:inline-block ml-3 text-sm">Dark Mode</span>
         </div>
       )}
     </div>
